95ef2cb32bc4404d1b51ef359830778ea12e6316
RomanJ55 <roman.jaro@web.de>
2021-01-24 01:06:16 +0100

    added scrolling to the main button

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ function toggleNavbar(collapseID) {
   document.getElementById(collapseID).classList.toggle("block");
 }
 
+const mb = document.getElementById("mainButton");
+mb.onclick = () => {
+  window.scrollBy({ top: 2400, left: 0, behavior: "smooth" });
+};
+
 AOS.init({
   delay: 200,
   duration: 1200,
