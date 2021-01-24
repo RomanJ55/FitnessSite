d5c09b9403f38e64b06a2ec6e37b4338a2712d15
RomanJ55 <roman.jaro@web.de>
2021-01-24 01:44:05 +0100

    removed tt from g-map, added some hover effects, added another button at the bottom to get back up

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ mb.onclick = () => {
   window.scrollBy({ top: 2400, left: 0, behavior: "smooth" });
 };
 
+const bb = document.getElementById("bottomButton");
+bb.onclick = () => {
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+};
+
 AOS.init({
   delay: 200,
   duration: 1200,
